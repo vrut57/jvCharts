431368198fc5378408a4e3c034c25bbd41579269
jlazar <jonlazar12@gmail.com>
2017-08-29 14:20:42 -0400

    cleaning up jv circle pack

--- src/visuals/jvPack.js
+++ src/visuals/jvPack.js
@@ -39,16 +39,14 @@ function getEventData() {
  * @returns [] of legend text
  */
 function setPackLegendData(dataTable) {
-    var legendArray = [];
-    var label = ''
-    for (var key in dataTable) {
+    var legendArray = [],
+        label = '';
+    for (let key in dataTable) {
         if (dataTable.hasOwnProperty(key)) {
             if (key === 'value') {
                 label = dataTable[key];
-            } else {
-                if (key !== 'tooltip 1') {
-                    legendArray.push(dataTable[key]);
-                }
+            } else if (key !== 'tooltip 1') {
+                legendArray.push(dataTable[key]);
             }
         }
     }
@@ -57,25 +55,24 @@ function setPackLegendData(dataTable) {
 }
 
 function paint() {
-    var chart = this;
+    var chart = this,
+        packMargins = {
+            top: 30,
+            right: 20,
+            bottom: 15,
+            left: 20
+        };
     chart._vars.color = chart.data.color;
 
     chart.currentData = chart.data;//Might have to move into method bc of reference/value relationship
-
-    var packMargins = {
-        top: 30,
-        right: 20,
-        bottom: 15,
-        left: 20
-    };
 
     //Generate SVG-legend data is used to determine the size of the bottom margin (set to null for no legend)
     chart.generateSVG(null, packMargins);
     chart.generateVerticalLegend('generatePack');
     chart.generatePack(chart.currentData);
-};
+}
 
-/** generatePack
+/**generatePack
  *
  * paints the pack on the chart
  * @params packData
@@ -88,65 +85,66 @@ function generatePack() {
         h = container.height,
         r = Math.min(h / 2, w / 3),
         margin = 20,
-        diameter = r * 2;
+        diameter = r * 2,
+        color,
+        root,
+        pack,
+        vis,
+        circle,
+        node,
+        view;
 
     chart.children = chart.data.chartData;
 
-    var color = d3.scaleOrdinal()
+    color = d3.scaleOrdinal()
         .range(chart.data.color
-            .map(function (c) { c = d3.rgb(c); c.opacity = 0.8; return c; }));
+            .map(c => {
+                c = d3.rgb(c);
+                c.opacity = 0.8;
+                return c;
+            }));
 
-    //  assigns the data to a hierarchy using parent-child relationships
-    var root = d3.hierarchy(chart.children, function (d) {
-        return d.children;
-    });
+    //assigns the data to a hierarchy using parent-child relationships
+    root = d3.hierarchy(chart.children, d => d.children);
 
-    var pack = d3.pack()
+    pack = d3.pack()
         .size([container.width, container.height])
         .padding(2);
 
     pack(root
-        .sum(function (d) {
-            return d.hasOwnProperty('children') ? 0 : d.name;
-        })
-        .sort(function (a, b) {
-            return b.height - a.height || b.value - a.value;
-        }))
+        .sum(d => d.hasOwnProperty('children') ? 0 : d.name)
+        .sort((a, b) => b.height - a.height || b.value - a.value))
         .descendants();
 
-    svg.selectAll(".pack").remove();
+    svg.selectAll('.pack').remove();
 
-    var vis = svg.append("g")
-        .attr("class", "pack")
-        .attr("transform", "translate(" + (w / 2) + "," + r + ")");
+    vis = svg.append('g')
+        .attr('class', 'pack')
+        .attr('transform', 'translate(' + (w / 2) + ',' + r + ')');
 
-    var circle = vis.selectAll("circle")
+    circle = vis.selectAll('circle')
         .data(root.descendants())
-        .enter().append("circle")
-        .attr("class", function (d) {
-            return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root";
-        })
-        .style("fill", function (d) {
+        .enter().append('circle')
+        .attr('class', d => d.parent ? d.children ? 'node' : 'node node--leaf' : 'node node--root')
+        .style('fill', d => {
             d.color = color(d.depth);
             return d.children ? color(d.depth) : null;
         })
-        .on("click", function (d) {
+        .on('click', function (d) {
             if (focus !== d) {
-                zoom(d), d3.event.stopPropagation();
+                zoom(d);
+                d3.event.stopPropagation();
             }
         })
-        .on("mouseover", function (d, i) {
+        .on('mouseover', function (d, i) {
             //Get tip data
-            // var tData = chart.data.tipData.get(d.data.name);
-            // tData.name = d.data.name;
-            // tData.color = d.color;
             var tipData = chart.setTipData(d, i);
             //Draw tip line
             chart.tip.generateSimpleTip(tipData, chart.data.dataTable);
             chart.tip.d = d;
             chart.tip.i = i;
         })
-        .on("mousemove", function (d, i) {
+        .on('mousemove', function (d, i) {
             if (chart.showToolTip) {
                 if (chart.tip.d === d && chart.tip.i === i) {
                     chart.tip.showTip(d3.event);
@@ -158,22 +156,14 @@ function generatePack() {
                 }
             }
         })
-        .on("mouseout", function (d) {
+        .on('mouseout', function () {
             chart.tip.hideTip();
         });
 
-    // var text = vis.selectAll("text")
-    //     .data(root.descendants())
-    //     .enter().append("text")
-    //     .attr("class", "label")
-    //     .style("display", function(d) {
-    //         return d.parent === root ? "inline" : "none";
-    //     });
+    node = svg.selectAll('circle,text');
 
-    var node = svg.selectAll("circle,text");
-
-    d3.select("body")
-        .on("click", function () {
+    d3.select('body')
+        .on('click', function () {
             zoom(root);
         });
 
@@ -181,53 +171,29 @@ function generatePack() {
 
     function zoom(d) {
         var focus = d;
-
         d3.transition()
             .duration(d3.event.altKey ? 7500 : 750)
-            .tween("zoom", function (d) {
+            .tween('zoom', function (d) {
                 var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
                 return function (t) {
                     zoomTo(i(t));
                 };
             });
-
-        // transition.selectAll("text")
-        //     .filter(function(d) {
-        //         return d.parent === focus || this.style.display === "inline";
-        //     })
-        //     .style("fill-opacity", function(d) {
-        //         return d.parent === focus ? 1 : 0;
-        //     })
-        //     .style("display", function(d) {
-        //         return d.parent === focus ? "inline" : "none";
-        //     })
-        //     .each("start", function(d) {
-        //         if (d.parent === focus) {
-        //            this.style.display = "inline";
-        //         }
-        //     })
-        //     .each("end", function(d) {
-        //         if (d.parent !== focus) {
-        //           this.style.display = "none";
-        //         }
-        //     });
     }
-    var view;
     function zoomTo(v) {
         var k = diameter / v[2];
 
         //set global zoom
         view = v;
 
-        node.attr("transform", function (d) {
+        node.attr('transform', d => {
             if (d && d.x && d.y) {
-                return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")";
+                return 'translate(' + (d.x - v[0]) * k + ',' + (d.y - v[1]) * k + ')';
             }
+            return '';
         });
 
-        circle.attr("r", function (d) {
-            return d.r * k;
-        });
+        circle.attr('r', d => d.r * k);
     }
 }
 
