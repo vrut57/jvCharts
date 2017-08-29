9346bd576f57c498d40cae3378f7db2ab1e61b64
jlazar <jonlazar12@gmail.com>
2017-08-29 14:31:53 -0400

    updating jv tip and vars

--- src/jvVars.js
+++ src/jvVars.js
@@ -8,16 +8,16 @@ function getDefaultOptions(userOptions = {}) {
 
     //General Styles/Attributes
     //CONST variables - try to not use in jvCharts as they do not have much context
-    _vars.GRAY = "#cccccc";
+    _vars.GRAY = '#cccccc';
     _vars.WHITE = '#FFFFFF';
     _vars.BLACK = '#000000';
     _vars.SMALL_STROKE_WIDTH = '1px';
     _vars.light = '#BBBBBB';
-    _vars.strokeWidth = "2px";
-    
+    _vars.strokeWidth = '2px';
+
     //Action Attributes
     _vars.highlightBorderColor = _vars.BLACK;
-    _vars.highlightBorderWidth = "2px";
+    _vars.highlightBorderWidth = '2px';
 
     //Component Specific Styles/Attributes
     _vars.thresholds = 'none'; //if not none, expected to be an array
@@ -29,9 +29,9 @@ function getDefaultOptions(userOptions = {}) {
     _vars.displayValues = false;
     _vars.toggleLegend = false;
 
-    // _vars.legendArrowColor = 'c2c2d6';
-    // _vars.legendArrowColor = '000000';
-    _vars.legendArrowColor = 'e7e7e7'; 
+    //_vars.legendArrowColor = 'c2c2d6';
+    //_vars.legendArrowColor = '000000';
+    _vars.legendArrowColor = 'e7e7e7';
 
     _vars.legendMax = 9;
     _vars.gridSize = 12;
@@ -45,61 +45,61 @@ function getDefaultOptions(userOptions = {}) {
     _vars.yLabelFontSize = 'none';
 
     //Heatmap Specific Styles/Attributes
-    _vars.color = "#ff0000";
+    _vars.color = '#ff0000';
     _vars.toggleLegend = false;
-    _vars.buckets = "10";
-    _vars.opacity = "10";
+    _vars.buckets = '10';
+    _vars.opacity = '10';
     _vars.colorLabel = 'none';
-    _vars.min = "0";
-    _vars.max = "10";
-    _vars.domainArray = "";
-    _vars.step = "1";
+    _vars.min = '0';
+    _vars.max = '10';
+    _vars.domainArray = '';
+    _vars.step = '1';
     _vars.quantiles = true;
     _vars.heatLegendSpacing = 2;
     _vars.heatGridSize = 20;
     _vars.colors = [
-      "#fbf2d2",
-      "#fdedb5",
-      "#fee7a0",
-      "#ffda84",
-      "#ffc665",
-      "#feb44e",
-      "#fea743",
-      "#fd9b3f",
-      "#fd8c3c",
-      "#fd7735",
-      "#fd602f",
-      "#fb4b29",
-      "#f43723",
-      "#ea241e",
-      "#e0161c",
-      "#d60b20",
-      "#c80324",
-      "#b10026",
-      "#870025",
-      "#620023"
+        '#fbf2d2',
+        '#fdedb5',
+        '#fee7a0',
+        '#ffda84',
+        '#ffc665',
+        '#feb44e',
+        '#fea743',
+        '#fd9b3f',
+        '#fd8c3c',
+        '#fd7735',
+        '#fd602f',
+        '#fb4b29',
+        '#f43723',
+        '#ea241e',
+        '#e0161c',
+        '#d60b20',
+        '#c80324',
+        '#b10026',
+        '#870025',
+        '#620023'
     ];
     _vars.clusterColors = [
-      "#00009C",
-      "#003399",
-      "#002FA7",
-      "#2A52BE",
-      "#0000FF",
-      "#8C92AC",
-      "#92A1CF",
-      "#C4C3D0",
-      "#CCCCFF",
-      "#F8F8FF",
-      "#FFFAFA",
-      "#F4C2C2",
-      "#FF6961",
-      "#FF5C5C",
-      "#FF1C00",
-      "#FF0800",
-      "#FF0000",
-      "#CD5C5C",
-      "#E34234",
-      "#D73B3E"
+        '#00009C',
+        '#003399',
+        '#002FA7',
+        '#2A52BE',
+        '#0000FF',
+        '#8C92AC',
+        '#92A1CF',
+        '#C4C3D0',
+        '#CCCCFF',
+        '#F8F8FF',
+        '#FFFAFA',
+        '#F4C2C2',
+        '#FF6961',
+        '#FF5C5C',
+        '#FF1C00',
+        '#FF0800',
+        '#FF0000',
+        '#CD5C5C',
+        '#E34234',
+        '#D73B3E'
     ];
 
     //pie specific
@@ -122,7 +122,6 @@ function getDefaultOptions(userOptions = {}) {
 
     //single axis specific
     _vars.singleAxisFillHoverColor = 'red';
-
 
     Object.assign(_vars, userOptions);
     return _vars;
